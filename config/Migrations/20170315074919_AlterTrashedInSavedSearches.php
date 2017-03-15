320acf6f954221e1117e39d41cfe3b4d07e028f0
George Constantinou <g.constantinou@qobo.biz>
2017-03-15 10:06:57 +0200

    set trash field as nullable (task #2897)

---
+++ config/Migrations/20170315074919_AlterTrashedInSavedSearches.php
@@ -0,0 +1,23 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AlterTrashedInSavedSearches extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('saved_searches');
+        debug($table);die;
+        $table->changeColumn('trashed', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->update();
+    }
+}
