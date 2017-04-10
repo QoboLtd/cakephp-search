09f052c0842263f79ba3d616dfc5c977f4582891
Michael Stepanov <michael@stepanoff.org>
2017-04-10 17:47:42 +0300

    Fixed phpcs errors (#task 806)

--- webroot/js/reports.js
+++ webroot/js/reports.js
@@ -6,16 +6,16 @@ $(document).ready(function () {
             var paramValue = $(this).val();
             var url = window.location.href;
             var hash = location.hash;
-            
+
             console.log('Chart type is ' + paramValue + '; url=' + url + '; hash=' + hash);
-           
+
             if (url.indexOf(paramName + "=") >= 0) {
                 var prefix = url.substring(0, url.indexOf(paramName));
                 var suffix = url.substring(url.indexOf(paramName));
-                suffix = suffix.substring(suffix.indexOf("=") + 1); 
-                suffix = (suffix.indexOf("&") >= 0) ? suffix.substring(suffix.indexOf("&")) : ""; 
+                suffix = suffix.substring(suffix.indexOf("=") + 1);
+                suffix = (suffix.indexOf("&") >= 0) ? suffix.substring(suffix.indexOf("&")) : "";
                 url = prefix + paramName + "=" + paramValue + suffix;
-            } else {    
+            } else {
                 url += (url.indexOf("?") < 0 ? "?" : "&") + paramName + "=" + paramValue;
             }
             window.location.href = url + hash;
