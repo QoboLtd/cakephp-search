067ac2872f13dc18d82b19394afda6166350635f
George Constantinou <georgec20001@gmail.com>
2017-09-01 17:52:17 +0300

    Added export configuration (Task #4333)

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,7 +3,20 @@ use Cake\Core\Configure;
 use Cake\Event\EventManager;
 use Search\Event\Model\WidgetsListener;
 
-// dashboards columns
-Configure::write('Search.dashboard.columns', ['Left Side', 'Right Side']);
+/**
+ * Plugin configuration
+ */
+// get app level config
+$config = Configure::read('Search');
+$config = $config ? $config : [];
+
+// load default plugin config
+Configure::load('Search.search');
+
+// overwrite default plugin config by app level config
+Configure::write('Search', array_replace_recursive(
+    Configure::read('Search'),
+    $config
+));
 
 EventManager::instance()->on(new WidgetsListener());
