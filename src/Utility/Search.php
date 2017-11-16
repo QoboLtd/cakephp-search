216d9b5bf048e08401d731bbd71c5453670a69d1
Michael Stepanov <michael@stepanoff.org>
2017-11-16 12:58:07 +0200

    Moved functionality to get child list items to the application level - task #4793

--- src/Utility/Search.php
+++ src/Utility/Search.php
@@ -11,6 +11,8 @@
  */
 namespace Search\Utility;
 
+use Cake\Event\Event;
+use Cake\Event\EventManager;
 use Cake\Http\ServerRequest;
 use Cake\ORM\Table;
 use Cake\ORM\TableRegistry;
@@ -19,6 +21,7 @@ use Cake\Utility\Inflector;
 use InvalidArgumentException;
 use Qobo\Utils\ModuleConfig\ConfigType;
 use Qobo\Utils\ModuleConfig\ModuleConfig;
+use Search\Event\EventName;
 use Search\Model\Entity\SavedSearch;
 use Search\Utility;
 use Search\Utility\BasicSearch;
@@ -31,16 +34,6 @@ class Search
      * Delete older than value
      */
     const DELETE_OLDER_THAN = '-3 hours';
-
-    /**
-     * DBlist constant
-     */
-    const LIST_TYPE_DBLIST = 'dblist';
-
-    /**
-     * File list constant
-     */
-    const LIST_TYPE_FILELIST = 'list';
 
     /**
      * Searchable table.
@@ -206,7 +199,17 @@ class Search
     {
         $result = $request->getData();
 
-        $result = $this->processSearchCriteria($result);
+        //$result = $this->processSearchCriteria($result);
+        $event = new Event((string)EventName::MODEL_SEARCH_CHILD_ITEMS(), $this, [
+            'criteria' => $result
+        ]);
+        EventManager::instance()->dispatch($event);
+
+        error_log(__METHOD__ . ": result before event --> " . print_r($result, true) . "\n", 3, '/tmp/search.log');
+
+        $result = $event->result ? $event->result : $result;
+
+        error_log(__METHOD__ . ": result after event --> " . print_r($result, true) . "\n", 3, '/tmp/search.log');
 
         $value = Hash::get($result, 'criteria.query');
 
@@ -219,123 +222,6 @@ class Search
         $result['aggregator'] = 'OR';
         $basicSearch = new BasicSearch($this->table, $this->user);
         $result['criteria'] = $basicSearch->getCriteria($value);
-
-        return $result;
-    }
-
-    /**
-     * processSearchCriteria method
-     *
-     * @param array $criteria for search
-     * @return array
-     */
-    protected function processSearchCriteria($criteria)
-    {
-        foreach ($criteria['criteria'] as $key => $val) {
-            $items = [];
-
-            if (is_array($val)) {
-                foreach ($val as $k => $v) {
-                    if ($v['operator'] != 'is') {
-                        continue;
-                    }
-
-                    array_push($items, $v['value']);
-
-                    $ret = $this->processChildren($v['value'], $v['type'], $key);
-
-                    if (!empty($ret)) {
-                        $items = array_merge($items, $ret);
-                    }
-
-                    $criteria['criteria'][$key][$k]['value'] = $items;
-                }
-            }
-        }
-
-        return $criteria;
-    }
-
-    /**
-     * getDbListChildren method
-     *
-     * @param string $parentId of parent item
-     * @param object $table where lists are stored
-     * @return array
-     */
-    private function getDbListChildren($parentId, $table)
-    {
-        $query = $table->find('all', [
-            'conditions' => ['parent_id' => $parentId],
-        ]);
-        $children = $query->toArray();
-
-        return $children;
-    }
-
-    /**
-     * getFileListChildren method
-     *
-     * @param string $parentValue of parent item
-     * @param string $listName for target list
-     * @return array with children elements or empty
-     */
-    private function getFileListChildren($parentValue, $listName)
-    {
-        if (strpos($listName, '.') !== false) {
-            list ($module, $name) = explode('.', $listName);
-            $listName = strtolower(Inflector::singularize($module) . '_' . Inflector::pluralize($name));
-        }
-
-        $moduleConfig = new ModuleConfig(ConfigType::LISTS(), null, $listName);
-        $listData = $moduleConfig->parse()->items;
-        $result = json_decode(json_encode($listData), true);
-
-        $list = [];
-        foreach ($result as $item) {
-            if ($item['value'] == $parentValue && !empty($item['children'])) {
-                foreach ($item['children'] as $child) {
-                    array_push($list, ['value' => $child['value']]);
-                }
-            }
-        }
-
-        return $list;
-    }
-
-    /**
-     * processChildren method
-     *
-     * @param string $value to search
-     * @param string $type - dblist of list stored in files
-     * @param string $listName to find children in
-     * @return array with childen items or empty
-     */
-    private function processChildren($value, $type = self::LIST_TYPE_DBLIST, $listName = '')
-    {
-        $result = [];
-        $list = [];
-
-        if ($type == static::LIST_TYPE_DBLIST) {
-            $table = TableRegistry::get('CsvMigrations.DblistItems');
-            $query = $table->find('all', [
-                'conditions' => ['value' => $value],
-            ]);
-            $item = $query->first();
-
-            $list = $this->getDbListChildren($item['id'], $table);
-        } elseif ($type == static::LIST_TYPE_FILELIST) {
-            $list = $this->getFileListChildren($value, $listName);
-        }
-
-        foreach ($list as $item) {
-            array_push($result, $item['value']);
-            $ret = $this->processChildren($item['value'], $type, $listName);
-
-            if (!empty($ret)) {
-                $result = array_merge($result, $ret);
-            }
-        }
 
         return $result;
     }
