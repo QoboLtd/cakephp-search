9af0a1e445388c257be16a87368d08cc8a35a72f
George Constantinou <georgec20001@gmail.com>
2017-12-08 18:05:01 +0200

    Always include primary key on search resultset (task #4928)

--- src/Controller/SearchTrait.php
+++ src/Controller/SearchTrait.php
@@ -145,9 +145,7 @@ trait SearchTrait
 
         $data = [];
         if ($resultSet instanceof ResultSet) {
-            if ((bool)$this->request->query('primary_key')) {
-                array_unshift($displayColumns, $table->aliasField($table->getPrimaryKey()));
-            }
+            array_unshift($displayColumns, $table->aliasField($table->getPrimaryKey()));
             $data = Utility::instance()->formatter($resultSet, $displayColumns, $table, $this->Auth->user());
         }
 
