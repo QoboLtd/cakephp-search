d340654422ef079e0aa96f4bf4334dd24ef0ec78
George Constantinou <georgec20001@gmail.com>
2017-12-21 18:13:23 +0200

    Overwrite display columns for 'group by' Search results (task #5096)

--- src/Controller/SearchTrait.php
+++ src/Controller/SearchTrait.php
@@ -118,13 +118,22 @@ trait SearchTrait
      */
     protected function getAjaxViewVars(array $searchData, Table $table, Search $search)
     {
-        $displayColumns = $searchData['display_columns'];
+        $displayColumns = [];
 
         $sortField = $this->request->query('order.0.column') ?: 0;
         $sortField = array_key_exists($sortField, $displayColumns) ?
             $displayColumns[$sortField] :
             current($displayColumns);
         $searchData['sort_by_field'] = $sortField;
+        if (empty($searchData['group_by'])) {
+            $displayColumns = $searchData['display_columns'];
+        }
+
+        if (!empty($searchData['group_by'])) {
+            list($prefix, ) = pluginSplit($searchData['group_by']);
+            $displayColumns = array_merge($displayColumns, (array)$searchData['group_by']);
+            $displayColumns[] = $prefix . '.' . Search::GROUP_BY_FIELD;
+        }
 
         $searchData['sort_by_order'] = $this->request->query('order.0.dir') ?: SearchOptions::getDefaultSortByOrder();
 
