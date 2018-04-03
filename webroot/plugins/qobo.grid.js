7e91866f910edaa2a0e7f2b78f6c68a7d0df3e73
George Constantinou <georgec20001@gmail.com>
2018-04-03 14:03:43 +0300

    Fix code-sniffer warnings (task #5719)

--- webroot/plugins/qobo.grid.js
+++ webroot/plugins/qobo.grid.js
@@ -17,28 +17,28 @@ new Vue({
         index:0,
         token: api_token // getting token from global variable into vue app.
     },
-    beforeMount: function() {
+    beforeMount: function () {
         this.getGridElements();
 
         this.getLayoutElements();
     },
-    mounted: function() {
+    mounted: function () {
         this.index = this.layout.length;
     },
-    beforeUpdate: function() {
-        this.$nextTick(function() {
+    beforeUpdate: function () {
+        this.$nextTick(function () {
             this.adjustBoxesHeight();
-        });     
+        });
     },
     watch: {
         // save all the visible options into dashboard var
         layout: {
-            handler: function() {
+            handler: function () {
                 var that = this;
                 this.dashboard = [];
 
-                if(this.layout.length > 0) {
-                    this.layout.forEach(function(element) {
+                if (this.layout.length > 0) {
+                    this.layout.forEach(function (element) {
                         that.dashboard.push({
                             i: element.i,
                             h: element.h,
@@ -79,7 +79,7 @@ new Vue({
 
             return 'fa-' + className;
         },
-        getLayoutElements: function() {
+        getLayoutElements: function () {
             let gridLayout = [];
 
             if (typeof grid_layout !== undefined ) {
@@ -88,7 +88,7 @@ new Vue({
             }
 
         },
-        getGridElements: function() {
+        getGridElements: function () {
             var that = this;
             let types = [];
             let models = [];
@@ -99,24 +99,24 @@ new Vue({
                 headers: {
                     'Authorization': 'Bearer ' + this.token
                 }
-            }).then(function(response) {
+            }).then(function (response) {
                 that.elements = response;
-                
-                that.elements.forEach(function(element){
+
+                that.elements.forEach(function (element) {
                     if (!types.includes(element.type)) {
-                      types.push(element.type);
+                        types.push(element.type);
                     }
 
                     if (element.type == 'saved_search' && !models.includes(element.data.model)) {
                         models.push(element.data.model);
                     }
                 });
-                
+
                 that.widgetTypes = types.sort();
                 that.searchModules = models.sort();
             });
         },
-        addItem: function(item) {
+        addItem: function (item) {
             let element = {
                 x: 0,
                 y: this.getLastRow(),
@@ -130,22 +130,22 @@ new Vue({
             this.layout.push(layoutElement);
             this.index = this.layout.length;
         },
-        removeItem: function(item) {
+        removeItem: function (item) {
             this.layout.splice(this.layout.indexOf(item), 1);
             this.index = this.layout.index;
         },
-        getUniqueId: function() {
+        getUniqueId: function () {
             return '_' + Math.random().toString(36).substr(2, 9);
         },
-        getLastRow: function() {
+        getLastRow: function () {
             let last = 0;
 
             if (!this.layout.length) {
                 return last;
             }
 
-            this.layout.forEach(function(element) {
-                if(element.y >= last) {
+            this.layout.forEach(function (element) {
+                if (element.y >= last) {
                     last = element.y;
                 }
             });
@@ -154,23 +154,21 @@ new Vue({
 
             return last;
         },
-        camelize: function(str) {
-            str = str.replace(/(?:\_|\W)(.)/g, function(match, chr) {
+        camelize: function (str) {
+            str = str.replace(/(?:\_|\W)(.)/g, function (match, chr) {
                 return ' ' + chr.toUpperCase();
             });
-            
+
             return str.charAt(0).toUpperCase() + str.slice(1);
         },
-        getActiveTab: function(type, defaultValue, cssClass) {
+        getActiveTab: function (type, defaultValue, cssClass) {
             return cssClass + ' ' + (type == defaultValue ? 'active' : '');
         },
-        adjustBoxesHeight: function()
-        {
-            var maxHeight = Math.max.apply(null, $("div.available-widget").map(function ()
-            {
+        adjustBoxesHeight: function () {
+            var maxHeight = Math.max.apply(null, $("div.available-widget").map(function () {
                 return $(this).height();
             }).get());
-            
+
             $("div.available-widget").height(maxHeight + 5);
         }
     }
