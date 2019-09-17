bc07c5ed2531a119c119719f0c9d934045e34463
George Constantinou <georgec20001@gmail.com>
2019-09-17 12:01:49 +0300

    Added initial search aggregates (task #12103)

---
+++ src/Aggregate/Average.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Search\Aggregate;
+
+use Cake\Database\Expression\FunctionExpression;
+use Cake\Database\FunctionsBuilder;
+
+final class Average extends AbstractAggregate
+{
+    const IDENTIFIER = 'avg';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getExpression() : FunctionExpression
+    {
+        return (new FunctionsBuilder())->avg($this->field);
+    }
+}
---
+++ tests/TestCase/Aggregate/MaximumTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Search\Test\TestCase\Aggregate;
+
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+use Search\Aggregate\Maximum;
+use Search\Criteria\Field;
+
+class MaximumTest extends TestCase
+{
+    public $fixtures = ['plugin.Search.articles'];
+
+    private $query;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->query = TableRegistry::getTableLocator()->get('Articles')->query();
+    }
+
+    public function tearDown() : void
+    {
+        unset($this->query);
+
+        parent::tearDown();
+    }
+
+    public function testApply() : void
+    {
+        $aggregate = new Maximum(new Field('priority'));
+
+        $result = $aggregate->apply($this->query);
+
+        $this->assertRegExp(
+            '/SELECT \(MAX\(priority\)\) AS "max%%priority"/',
+            $result->sql()
+        );
+    }
+}
