6a75c926ae603006da4f1dcfbd408f78160fdf53
George Constantinou <georgec20001@gmail.com>
2019-09-17 12:24:16 +0300

    Added priority column on articles fixture (task #12103)

--- tests/Fixture/ArticlesFixture.php
+++ tests/Fixture/ArticlesFixture.php
@@ -24,6 +24,7 @@ class ArticlesFixture extends TestFixture
         'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         'published' => ['type' => 'boolean', 'length' => null, 'null' => false, 'default' => false, 'comment' => '', 'precision' => null],
+        'priority' => ['type' => 'decimal', 'length' => null, 'null' => false, 'default' => false, 'comment' => '', 'precision' => null],
         '_constraints' => [
             'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
         ],
@@ -48,6 +49,7 @@ class ArticlesFixture extends TestFixture
             'created' => '2016-12-27 12:25:30',
             'modified' => '2017-05-22 08:21:50',
             'published' => 1,
+            'priority' => 10,
         ],
         [
             'id' => '00000000-0000-0000-0000-000000000002',
@@ -57,6 +59,7 @@ class ArticlesFixture extends TestFixture
             'created' => '2016-04-27 08:21:54',
             'modified' => '2016-04-27 08:21:54',
             'published' => 0,
+            'priority' => 20,
         ],
         [
             'id' => '00000000-0000-0000-0000-000000000003',
@@ -66,6 +69,7 @@ class ArticlesFixture extends TestFixture
             'created' => '2019-07-25 16:00:13',
             'modified' => '2019-07-25 16:00:13',
             'published' => 1,
+            'priority' => 30,
         ]
     ];
 }
