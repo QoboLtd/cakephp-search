4415da47bf6f6f4704ab2e65a9c1626ca92dc6b7
George Constantinou <georgec20001@gmail.com>
2019-10-04 16:35:37 +0300

    Cast value object to string (task #12103)

--- src/Service/Search.php
+++ src/Service/Search.php
@@ -149,7 +149,7 @@ final class Search
         $this->applyJoins();
 
         if (null !== $this->groupBy) {
-            $this->query->group($this->groupBy);
+            $this->query->group((string)$this->groupBy);
         }
 
         if ($this->orderBy) {
