44506e01ec35f6d6a532b94b920b11d7195cd8b8
George Constantinou <georgec20001@gmail.com>
2019-10-11 12:38:55 +0300

    Adjust fixture records to match new schema structure (task #12103)

--- tests/Fixture/SavedSearchesFixture.php
+++ tests/Fixture/SavedSearchesFixture.php
@@ -19,13 +19,18 @@ class SavedSearchesFixture extends TestFixture
     public $fields = [
         'id' => ['type' => 'uuid', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         'name' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
-        'user_id' => ['type' => 'uuid', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'user_id' => ['type' => 'uuid', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         'model' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
-        'content' => ['type' => 'text', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
-        'system' => ['type' => 'boolean', 'length' => null, 'null' => false, 'default' => false, 'comment' => '', 'precision' => null],
+        'content' => ['type' => 'text', 'length' => 4294967295, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
         'created' => ['type' => 'datetime', 'null' => false],
         'modified' => ['type' => 'datetime', 'null' => false],
         'trashed' => ['type' => 'datetime', 'null' => true],
+        'conjunction' => ['type' => 'string', 'length' => 10, 'null' => false, 'default' => 'AND', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'criteria' => ['type' => 'text', 'length' => 4294967295, 'null' => false, 'default' => '\'\'', 'comment' => '', 'precision' => null],
+        'fields' => ['type' => 'text', 'length' => 4294967295, 'null' => false, 'default' => '\'\'', 'comment' => '', 'precision' => null],
+        'group_by' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => '', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'order_by_field' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => '', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'order_by_direction' => ['type' => 'string', 'length' => 10, 'null' => false, 'default' => 'DESC', 'comment' => '', 'precision' => null, 'fixed' => null],
         '_constraints' => [
             'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
         ],
@@ -70,9 +75,14 @@ class SavedSearchesFixture extends TestFixture
                     "limit": "10"
                 }
             }',
+            'conjunction' => 'OR',
+            'criteria' => '{ "Dashboards.first_name": [{ "type": "string", "operator": "contains", "value": "jo" }] }',
+            'fields' => '["first_name", "last_name", "street", "city"]',
+            'group_by' => '',
+            'order_by_field' => 'Dashboards.first_name',
+            'order_by_direction' => 'desc',
             'created' => '2016-07-01 10:39:23',
             'modified' => '2016-07-01 10:41:31',
-            'system' => false,
             'trashed' => null,
         ],
         [
@@ -118,9 +128,14 @@ class SavedSearchesFixture extends TestFixture
                     "limit": 10
                 }
             }',
+            'conjunction' => 'OR',
+            'criteria' => '{ "Dashboards.name": [{ "type": "string", "operator": "contains", "value": "foo" }] }',
+            'fields' => '["Dashboards.name"]',
+            'group_by' => '',
+            'order_by_field' => 'Dashboards.name',
+            'order_by_direction' => 'desc',
             'created' => '2016-07-01 10:39:22',
             'modified' => '2016-07-01 10:41:31',
-            'system' => true,
             'trashed' => null,
         ],
         [
@@ -164,9 +179,14 @@ class SavedSearchesFixture extends TestFixture
                     "sort_by_order": "desc"
                 }
             }',
+            'conjunction' => 'OR',
+            'criteria' => '{ "Articles.title": [{ "type": "string", "operator": "contains", "value": "article" }] }',
+            'fields' => '["Articles.title"]',
+            'group_by' => '',
+            'order_by_field' => 'title',
+            'order_by_direction' => 'desc',
             'created' => '2016-07-01 10:39:23',
             'modified' => '2016-07-01 10:41:31',
-            'system' => false,
             'trashed' => null,
         ],
         [
@@ -194,9 +214,14 @@ class SavedSearchesFixture extends TestFixture
                     "limit": 10
                 }
             }',
+            'conjunction' => 'OR',
+            'criteria' => '{ "Dashboards.name": [{ "type": "string", "operator": "contains", "value": "user" }] }',
+            'fields' => '["Dashboards.name"]',
+            'group_by' => '',
+            'order_by_field' => 'Dashboards.name',
+            'order_by_direction' => 'desc',
             'created' => '2016-07-01 10:39:23',
             'modified' => '2016-07-01 10:41:31',
-            'system' => false,
             'trashed' => '2016-07-01 10:41:31',
         ],
         [
@@ -242,9 +267,14 @@ class SavedSearchesFixture extends TestFixture
                     "group_by": "Articles.author_id"
                 }
             }',
+            'conjunction' => 'OR',
+            'criteria' => '{ "Articles.title": [{ "type": "string", "operator": "contains", "value": "article" }] }',
+            'fields' => '["Articles.author_id","COUNT(Articles.title)"]',
+            'group_by' => 'Articles.author_id',
+            'order_by_field' => 'title',
+            'order_by_direction' => 'desc',
             'created' => '2016-07-01 10:39:23',
             'modified' => '2016-07-01 10:41:31',
-            'system' => false,
             'trashed' => null,
         ],
         [
@@ -253,9 +283,14 @@ class SavedSearchesFixture extends TestFixture
             'user_id' => '00000000-0000-0000-0000-000000000001',
             'model' => 'Articles',
             'content' => '{"saved":{"criteria":{"Articles.title":[{"type":"string","operator":"contains","value":"article"}]},"aggregator":"OR","display_columns":["Articles.title","Articles.content"],"sort_by_field":"title","sort_by_order":"desc"},"latest":{"criteria":{"Articles.title":[{"type":"string","operator":"contains","value":"article"}]},"aggregator":"OR","display_columns":["Articles.title","Articles.content"],"sort_by_field":"title","sort_by_order":"desc"}}',
+            'conjunction' => 'OR',
+            'criteria' => '{ "Articles.title":[{ "type":"string", "operator":"contains", "value":"article" }] }',
+            'fields' => '["Articles.title","Articles.author_id","Articles.content","Authors.name","Articles.status","Authors.type"]',
+            'group_by' => '',
+            'order_by_field' => 'title',
+            'order_by_direction' => 'desc',
             'created' => '2016-07-01 10:39:23',
             'modified' => '2016-07-01 10:41:31',
-            'system' => false,
             'trashed' => null,
         ],
     ];
