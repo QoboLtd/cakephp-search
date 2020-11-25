4f15dd1ab48149d435a4d28805b0405a24f30712
Nicos Panayides <nicosp@gmail.com>
2020-11-25 12:50:05 +0200

    Whitespace fixes (task #18184)

--- config/Migrations/20201118104505_AddGroupsToDashboards.php
+++ config/Migrations/20201118104505_AddGroupsToDashboards.php
@@ -1,4 +1,5 @@
 <?php
+
 use Migrations\AbstractMigration;
 
 class AddGroupsToDashboards extends AbstractMigration
@@ -8,6 +9,7 @@ class AddGroupsToDashboards extends AbstractMigration
      *
      * More information on this method is available here:
      * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
      * @return void
      */
     public function change()
@@ -15,7 +17,7 @@ class AddGroupsToDashboards extends AbstractMigration
         $this->table('qobo_search_dashboards')
             ->addColumn('group_id', 'uuid', [
             'limit' => 36,
-            'null' => true
+            'null' => true,
             ])
             ->update();
     }
